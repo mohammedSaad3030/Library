cfc41b7b6d47750f5f0ab151b26e19e1deebb830
mohammedSaad3030 <54086365+mohammedSaad3030@users.noreply.github.com>
2019-09-18 16:44:11 +0200

    Update Library.cpp

--- Library.cpp
+++ Library.cpp
@@ -5,14 +5,62 @@ Ctrl + D  repeat the line
 Ctrl + Shift + C on codeblocks  || Ctrl + Shift + / in clion    line comment
 Ctrl + Shift + X on codeblocks  || Ctrl + Shift + / in clion    line uncomment
  */
+
 ///minimization : (st + ed) / 2;
 ///maximization : (st + ed + 1)/ 2;
+
+///a = 97  z = 122
+///A = 65  Z = 90
+
+#include <bits/stdtr1c++.h>
+
+using namespace std;
+
+#define endl            "\n"
+#define pb              push_back
+#define all(v)			((v).begin()), ((v).end())
+#define sz(v)			((int)((v).size()))
+#define ll              long long
+#define ld              long double
+#define rep(i,n)        for(int i=0 ; i<sz(n) ; i++)
+#define lp(i, n)        for(int i = 0 ; (int)i<n ; i++)
+#define loop(i, x, n)   for(int i=x ; (int)i<=n ; i++)
+
+typedef vector<int> vi;
+typedef deque<int> de;
+typedef map<int, int> imap;
+typedef map<char, int> cmap;
+
 const double PI = 3.14159265358979323846;
+const double PI = acos(-1.0);
 const double EPSILON = 0.001;
+const double EPSILON = 1e-9;
 const ll INF = 1e15;
 const int MOD = 1e9 + 7;
-const double EPS = 1e-9;
-const double PI = acos(-1.0);
+
+///memset(vis, 0, sizeof vis);
+
+///double equality
+bool Equal(double a, double b)
+{
+    return fabs(a - b) < EPSILON;
+}
+
+///Double whole comparison
+///Return 0 for a==b, 1 for a > b, -1 for a < b
+int comp_double(double a, double b)
+{
+	//If very small difference, then equal
+	if(fabs(a - b) <= EPSILON
+		return 0;
+	return a < b ? -1 : 1;
+}
+
+///Ceil division
+ll ceilDiv(ll a, ll b)
+{
+    return (a + b - 1)/b;
+}
 
 ///gcd
 int gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b ); }
@@ -24,43 +72,155 @@ int summ(int i, int n)
     return (summ(1, n) - summ(1, i-1));
 }
 
-///convert string tolower or toupper letters
+///Size of an array
+int n = sizeof(arr)/sizeof(arr[0]); 
+
+///transform(Iterator inputBegin, Iterator inputEnd, Iterator OutputBegin, unary_operation);
+
+///Convert string tolower or toupper letters
 transform(su.begin(), su.end(), su.begin(), ::toupper); 
+
+///Increment all elements of an array
+transform(arr, arr+n, arr, increment);
+
+///Add two arrays and save the result in res array
+transform(arr1, arr1+n, arr2, res, plus<int>());
+
 
 
 ///fining all dividors of a number g
 vector<int> dividors;
 for(int i = 1 ; i <= g/i ; i++)
+{
+    if(g%i == 0)
     {
-        if(g%i == 0)
-        {
-            dividors.push_back(i);
-            if(g/i != i) dividors.push_back(g/i);;
-        }
+        dividors.push_back(i)
+        if(g/i != i) dividors.push_back(g/i);;
     }
+}///You can sort the dividors vector (if you need the dividors to be sorted) after you finish generating it.
 
-///double comparison
-bool Equal(double a, double b)
-{
-    return fabs(a - b) < EPSILON;
-}
 
-///Ceil division
-ll ceilDiv(ll a, ll b)
-{
-    return (a + b - 1)/b;
-}
 
-void findPermutations(ll arr[], int n)
+///Permutations 
+https://www.geeksforgeeks.org/write-a-c-program-to-print-all-permutations-of-a-given-string/
+///A string of length n has 00n! permutation.
+
+int ctr; ///Number of permutations.
+void findPermutations(int arr[], int n)
 {
     sort(arr, arr + n);
     do
     {
         ctr++;
+        lp(i, n)
+        {
+            cout << arr[i] << " " ;
+        }
+        cout << endl;
     } while (next_permutation(arr, arr + n));
 }
 
+int ctr; ///Number of permutations.
+void findPermutations(string str)
+{
+    sort(all(str));	///all(str) : str.begin(), str.end()
+    do
+    {
+        ctr++;
+        cout << str << endl;
+    } while (next_permutation(all(str)));
+}
 
+///A string of length n has n! permutation.
+void permute(string a, int l, int r)
+{
+    if (l == r)
+        cout<<a<<endl;
+    else
+    {
+        // Permutations made
+        for (int i = l; i <= r; i++)
+        {
+            // Swapping done
+            swap(a[l], a[i]);
+
+            // Recursion called
+            permute(a, l+1, r);
+
+            //backtrack
+            swap(a[l], a[i]);
+        }
+    }
+}
+
+
+///Fibonacci
+///The sequence Fn of Fibonacci numbers is defined by the recurrence relation (Fn = Fn-1 + Fn-2)
+///with seed values (F0 = 0 and F1 = 1.)
+
+///Get n-th Fibonacci Number.
+int fib(int n)
+{
+    if (n <= 1)
+        return n;
+    return fib(n-1) + fib(n-2);
+}
+
+///Generate Fibonacci Series up to n number of terms
+vi fibvec;
+void fibSeries(int n)
+{
+    int t1 = 0, t2 = 1, nextTerm = 0;
+    for (int i = 1; i <= n; ++i)
+    {
+        // Prints the first two terms.
+        if(i == 1)
+        {
+            fibvec.pb(t1);
+            continue;
+        }
+        if(i == 2)
+        {
+            fibvec.pb(t2);
+            continue;
+        }
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+
+        fibvec.pb(nextTerm);
+    }
+}
+
+
+///Bits manipulation (Bitmasks)
+void printSet(int s)
+{
+  cout << "s = " << s << endl;
+  stack<int> st;
+  while (s)
+    st.push(s % 2), s /= 2;
+  while (!st.empty())                         // to reverse the print order
+    {cout << st.top(); st.pop();}
+  cout << endl;
+}
+#define isOn(S, j) (S & (1 << j))
+#define setBit(S, j) (S |= (1 << j))
+#define clearBit(S, j) (S &= ~(1 << j))
+#define toggleBit(S, j) (S ^= (1 << j))
+#define lowBit(S) (S & (-S))
+#define setAll(S, n) (S = (1 << n) - 1)
+
+#define modulo(S, N) ((S) & (N - 1))   // returns S % N, where N is a power of 2
+#define isPowerOfTwo(S) (!(S & (S - 1)))
+#define nearestPowerOfTwo(S) ((int)pow(2.0, (int)((log((double)S) / log(2.0)) + 0.5)))
+#define turnOffLastBit(S) ((S) & (S - 1))
+#define turnOnLastZero(S) ((S) | (S + 1))
+#define turnOffLastConsecutiveBits(S) ((S) & (S + 1))
+#define turnOnLastConsecutiveZeroes(S) ((S) | (S - 1))
+
+
+///DFS
 void dfs(int node)
 {
 	visited[node] = true;
@@ -92,7 +252,6 @@ bool isSingleComponent() {
     return true;
 }
 
-
 void dfs_EdgeClassification(int node)
 {
 	start[node] = timer++;
@@ -114,7 +273,6 @@ void dfs_EdgeClassification(int node)
 
 	finish[node] = timer++;
 }
-
 
 int cnt = 0;
 void cntReachalbleCells(int r, int c)
@@ -157,7 +315,6 @@ void farthestNode(int node)
     << ", and the number of nodes on that path is : " << mxd << "\n";
 }
 
-
 /// farthest path in the tree as all
 void dfs(int node, int d = 1)
 {
@@ -173,15 +330,52 @@ void dfs(int node, int d = 1)
         mxnode = node;
     }
 }
-int main () /// dfs on any node you want, this will bring an edged node (نود عالحافة), then dfs on that node.
-{
- 	memset(vis, 0, sizeof vis);
-	msx = 0; mxnode = -1;
-	dfs(1);
-	memset(vis, 0, sizeof vis);
-	mxd= 0 ;
-	dfs(mxnode);
-	cout << mxd-1 << "\n;
-}
 
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);	
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+    	freopen("output.txt", "w", stdout);
+    	#else
+    	#endif
+	
+	///Bits manipulation 
+	int s = 34;
+	printSet(s);
+	cout << "2. Multiply s by 2, then divide S by 4 (2x2), then by 2\n";
+	s = 34; printSet(s);
+	s = s << 1; printSet(s);
+	s = s >> 2; printSet(s);
+	s = s >> 1; printSet(s);
+	cout << endl;
+
+	cout << "3. Set/turn on the 3-th item of the set\n";
+	s = 34; printSet(s);
+	setBit(s, 3); printSet(s);
+	cout << endl;
+
+	int T;
+	cout << "4. Check if the 3-th and then 2-nd item of the set is on?\n";
+	s = 42; printSet(s);
+	T = isOn(s, 3); if(T) cout << "ON\n"; else cout << "OFF\n";
+	T = isOn(s, 2); if(T) cout << "ON\n"; else cout << "OFF\n";
+
+	cout << "5. Clear/turn off the 1-st item of the set\n";
+	s = 42; printSet(s);
+	clearBit(s, 1); printSet(s);
+	cout << endl;
+
+	cout << "6. Toggle the 2-nd item and then 3-rd item of the set\n";
+	s = 40; printSet(s);
+	toggleBit(s, 2); printSet(s);
+	toggleBit(s, 3); printSet(s);
+	cout << endl;
+
+	cout << "7. Check the first bit from right that is on\n";
+	s = 40; printSet(s);
+	T = lowBit(s); cout << "T = " << T << " (this is always a power of 2)\n";
+	s = 52; printSet(s);
+	T = lowBit(s); cout << "T = " << T << " (this is always a power of 2)\n";
+	cout << endl;
 
